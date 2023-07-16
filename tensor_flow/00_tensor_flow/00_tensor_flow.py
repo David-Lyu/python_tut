b9f0fcd312633089acaebe1186d960a02243fc3c
David-Lyu <lyu.david@yahoo.com>
2023-07-15 23:16:58 -0700

    created tensors

--- tensor_flow/00_tensor_flow/00_tensor_flow.py
+++ tensor_flow/00_tensor_flow/00_tensor_flow.py
@@ -1,3 +1,62 @@
+import numpy as np
+# import pandas
 import tensorflow as tf
 
+
+print(tf.config.list_physical_devices())
+
 print(tf.__version__)
+
+# Creating tensors with tf.constant()
+
+scalar = tf.constant(7);
+## Returns: <tf.Tensor: shape=(), dtype=int32, numpy=7>
+scalar
+
+
+## Checks the number of dimensions of a tensors(ndim)
+## Output: 0
+scalar.ndim
+
+## Create a vector
+vector = tf.constant([10,10])
+## <tf.Tensor: shape(2,), dtype=int32, numpy=array([10,10], dtype=int32)>
+vector
+
+## Output: 1
+vector.ndim
+
+## Create a matrix
+matrix = tf.constant([[10,7],[7,10]])
+matrix
+
+## <tf.Tensor: shape=(2,2), dtype=int32, numpy=array([[10,7],[7,10]], dtype=int32)
+
+## Output: 2
+matrix.ndim
+
+## Create another matrix
+matrix2 = tf.constant([[10.,7.],[3.,2.],[8.,9.]],dtype=tf.float16)
+##Output: 2
+matrix2
+
+tensor = tf.constant([
+  [
+    [1,2,3],
+    [4,5,6]
+  ],
+  [
+    [7,8,9],
+    [10,11,12]
+  ],
+  [
+    [13,14,15],
+    [16,17,18]
+  ]
+])
+
+print(tensor)
+## Output: 3
+tensor.ndim
+
+### ALL matrix, vecorts, scalar will be still referenced as tensors
